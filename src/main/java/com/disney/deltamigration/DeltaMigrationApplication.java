6234c495845484d39d371eb2926be9105dabef53
alexmax198361 <107352757+alexmax198361@users.noreply.github.com>
2022-10-15 00:34:42 +0300

    init commit

---
+++ src/main/java/com/disney/deltamigration/DeltaMigrationApplication.java
@@ -0,0 +1,13 @@
+package com.disney.deltamigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaMigrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaMigrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/disney/deltamigration/DeltaMigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.deltamigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaMigrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
